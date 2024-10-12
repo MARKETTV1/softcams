83cb12a090df8612cd164a7ebacd8eda13b20e47
MARKETTV1 <aliridakarim8@gmail.com>
2024-10-12 14:59:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 
-wget -O /etc/etc/init.d/softcam.PowerCAM https://raw.githubusercontent.com/MARKETTV1/PowerCAM-PY3.12.6/refs/heads/main/softcam.PowerCAM && killall -9 enigma2
+wget -O /etc/etc/init.d/softcam.PowerCAM "https://raw.githubusercontent.com/MARKETTV1/PowerCAM-PY3.12.6/refs/heads/main/softcam.PowerCAM"
