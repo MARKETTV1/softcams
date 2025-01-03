a562830c4e9e3a0d95657c968ee7c72889c22369
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-03 13:23:11 +0100

    Update suptv.sh

--- suptv.sh
+++ suptv.sh
@@ -5,7 +5,6 @@ echo "#####################################"
 wait
 wait
 wait
-rm -r /usr/lib/enigma2/python/Plugins/Extensions/suptv
 wait
 wget -O /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk "https://github.com/MARKETTV1/softcams/raw/refs/heads/main/enigma2-plugin-extensions-suptv_4.1_all.ipk"
 wait
