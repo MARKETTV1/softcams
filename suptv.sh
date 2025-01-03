63ab0f47620dd11bfcf1376fbb627382b84542a7
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-03 13:01:56 +0100

    Update suptv.sh

--- suptv.sh
+++ suptv.sh
@@ -1,34 +1,23 @@
-
+echo "#####################################"
 echo " ✨✨✨✨SUPTV ✨✨✨✨"
 echo " ✨✨✨✨OPENPLI9.X & 8.X ✨✨✨✨"
-
+echo "#####################################"
 wait
 wait
 wait
-wait
-
 rm -r /usr/lib/enigma2/python/Plugins/Extensions/suptv
-
 wait
-
 wget -O /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk "https://github.com/MARKETTV1/softcams/raw/refs/heads/main/enigma2-plugin-extensions-suptv_4.1_all.ipk"
-
 wait
-
 opkg update && opkg install --force-overwrite /tmp/*.ipk
-
 wait
-
 if [ $? -eq 0 ]; then
    echo "🎉 Installation was successful! The package is ready for use. 🚀"
 else
     echo "❌ An error occurred during installation! Please try again. 😔"
 fi
-
 rm -r /tmp/*.ipk
-
 echo " ✨✨✨✨SUPTV ✨✨✨✨"
-
 init 4
 init 3
 
