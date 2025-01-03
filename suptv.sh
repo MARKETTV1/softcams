26f04e779650fcf38459011dc83994b7416a9c9f
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-03 11:54:04 +0100

    Update suptv.sh

--- suptv.sh
+++ suptv.sh
@@ -10,9 +10,10 @@ opkg install /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk
 
 # التحقق من التثبيت
 if [ $? -eq 0 ]; then
-    echo "The installation was successful."
+    echo "The installation was successful. 🚀 "
+    
 else
-    echo "There was an error during the installation."
+    echo "There was an error during the installation.  😔  "
 fi
 
 # تنظيف الملفات المؤقتة
