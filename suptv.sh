5f9b8bdeabdf5b5c22a6e5fc7465024360f183e0
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-03 10:27:57 +0100

    Update suptv.sh

--- suptv.sh
+++ suptv.sh
@@ -1,1 +1,17 @@
-  https://github.com/MARKETTV1/softcams/blob/main/enigma2-plugin-extensions-suptv_4.1_all.ipk
+ #!/bin/bash
+
+# تحميل الحزمة باستخدام wget
+echo "تحميل الحزمة من الرابط..."
+wget -O /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk "https://github.com/MARKETTV1/softcams/blob/main/enigma2-plugin-extensions-suptv_4.1_all.ipk?raw=true"
+
+# تثبيت الحزمة باستخدام opkg
+echo "تثبيت الحزمة..."
+opkg install /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk
+
+# تنظيف الملفات المؤقتة
+echo "تنظيف الملفات المؤقتة..."
+rm -f /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk
+
+echo "تم التثبيت بنجاح!"
+
+  
