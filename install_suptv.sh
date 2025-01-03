562b70eecfb433133d91a0c62b71941edfb538b5
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-03 14:54:30 +0100

    Update install_suptv.sh

--- install_suptv.sh
+++ install_suptv.sh
@@ -9,4 +9,5 @@ wget -O /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk https://github.com/MARK
 opkg install /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk
 
 # Optionnel: Redémarrer Enigma2 ou recharger le service
-init 4 && init 3
+init 4
+init 3
