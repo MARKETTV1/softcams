4b12fd999cee1c74ca02d00d08e9a05d353f151d
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-23 17:45:51 +0100

    Update install_suptv.sh

--- install_suptv.sh
+++ install_suptv.sh
@@ -3,7 +3,7 @@
 opkg remove enigma2-plugin-extensions-suptv
 
 # Téléchargement du fichier .ipk depuis GitHub
-wget -O /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk https://github.com/MARKETTV1/softcams/raw/refs/heads/main/enigma2-plugin-extensions-suptv_4.1_all.ipk
+wget -O /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk https://team4e2.xyz/apk/openpli.suptv_1.4_all.ipk
 
 # Installation du fichier .ipk
 opkg install /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk
