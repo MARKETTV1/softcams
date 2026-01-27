4f553b1826769da83e80e0c3638db7787271157f
MARKETTV1 <aliridakarim8@gmail.com>
2026-01-27 02:47:24 +0100

    Update ncam.sh

--- ncam.sh
+++ ncam.sh
@@ -1,2 +1,15 @@
-https://github.com/MARKETTV1/softcams/raw/refs/heads/main/enigma2-plugin-softcams-ncam_V15.6-r0_all.ipk
 
+#!/bin/sh
+
+opkg remove enigma2-plugin-softcams-ncam_V15.6-r0_all.ipk
+
+# Téléchargement du fichier .ipk depuis GitHub
+wget -O /tmp/enigma2-plugin-softcams-ncam_V15.6-r0_all.ipk https://github.com/MARKETTV1/softcams/raw/refs/heads/main/enigma2-plugin-softcams-ncam_V15.6-r0_all.ipk
+
+# Installation du fichier .ipk
+opkg install /tmp/enigma2-plugin-softcams-ncam_V15.6-r0_all.ipk
+
+# Optionnel: Redémarrer Enigma2 ou recharger le service
+sleep 3
+killall -9 enigma2
+exit 0
