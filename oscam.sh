dcd147acb4649437c48f2a830efab324bae0d8d1
MARKETTV1 <aliridakarim8@gmail.com>
2026-01-28 01:03:41 +0100

    Update oscam.sh

--- oscam.sh
+++ oscam.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-opkg remove enigma2-plugin-softcams-oscam_11939-emu-r802_all.ipk
+opkg remove enigma2-softcams-oscam-all-images_11940-emu-802-arm+mips_all.ipk
 
 # Téléchargement du fichier .ipk depuis GitHub
-wget -O /tmp/enigma2-plugin-softcams-oscam_11939-emu-r802_all.ipk https://github.com/MARKETTV1/softcams/raw/refs/heads/main/enigma2-plugin-softcams-oscam_11939-emu-r802_all.ipk
+wget -O /tmp/enigma2-softcams-oscam-all-images_11940-emu-802-arm+mips_all.ipk https://github.com/MARKETTV1/softcams/raw/refs/heads/main/enigma2-softcams-oscam-all-images_11940-emu-802-arm+mips_all.ipk
 
 # Installation du fichier .ipk
-opkg install /tmp/enigma2-plugin-softcams-oscam_11939-emu-r802_all.ipk
+opkg install /tmp/enigma2-softcams-oscam-all-images_11940-emu-802-arm+mips_all.ipk
 
 # Optionnel: Redémarrer Enigma2 ou recharger le service
 sleep 3
